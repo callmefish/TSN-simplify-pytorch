65786bb24e8e614d354c17b4960132abc0d3b86c
Zhiyu Yan <31463079+callmefish@users.noreply.github.com>
2020-05-02 23:53:17 -0500

    Update test.sh
    a little change

--- test.sh
+++ test.sh
@@ -3,15 +3,15 @@
 video name, json named by your UIN
 GIT_REPO_URL="https://github.com/callmefish/TSN-simplify-pytorch.git"
 REPO="TSN-simplify-pytorch"
-VIDEO="test_video1.mp4"
+VIDEO="./sample_video/sample_video_04(S).mp4"
 UIN_JSON="529005218.json"
 UIN_JPG="529005218.jpg"
 git clone $GIT_REPO_URL
-gsutil cp gs://ucf101_for_rar/sample_video.zip /TSN-simplify-pytorch/
+gsutil cp gs://ucf101_for_rar/sample_video.zip /$REPO
 cd $REPO
 unzip sample_video.zip
-gsutil cp gs://ucf101_for_rar/475_inceptionv4__flow_model_best.pth.tar /TSN-simplify-pytorch/
-gsutil cp gs://ucf101_for_rar/475_inceptionv4_rgb_model_best.pth.tar /TSN-simplify-pytorch/
+gsutil cp gs://ucf101_for_rar/475_inceptionv4__flow_model_best.pth.tar ./
+gsutil cp gs://ucf101_for_rar/475_inceptionv4_rgb_model_best.pth.tar ./
 #Replace this line with commands for running your test python file.
 echo $VIDEO
 python test.py --video_name $VIDEO
